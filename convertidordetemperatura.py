12759ae2cff5a32c646913b6dd36fc51aeb73473
yairjr-code <eduardojad1102@gmail.com>
2025-02-22 10:33:55 -0500

    ejercicio de covertidor de fahrenheit a celsius temperatura # utilizando input para digitar dato en cosola.

---
+++ convertidordetemperatura.py
@@ -0,0 +1,13 @@
+# ejercicio de covertidor de fahrenheit a celsius temperatura 
+# utilizando input para digitar dato en cosola.
+#  
+
+
+
+f = float(input("Digita la temperatura en °F: "))
+
+c = ((f)-32)//1.8
+
+print (c)
+
+
